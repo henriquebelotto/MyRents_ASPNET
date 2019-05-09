5b31a00e6341ca7b16b94943e8f5a670c21dc98f
henriquebelotto <henriquebelotto@gmail.com>
2019-05-09 13:23:15 -0400

    Added Properties to Movies and Added MovieGenres table

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,22 @@ namespace Vidly.Models
     // POCO
     public class Movie
     {
-        public string Name { get; set; }
         public int Id { get; set; }
 
-        // write prop and press tab and it will easily generate properties
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public MovieGenre Genre { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        [Required]
+        public int NumberInStock { get; set; }
+
     }
 }
---
+++ Vidly/Models/MovieGenre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class MovieGenre
+    {
+        public Byte Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
