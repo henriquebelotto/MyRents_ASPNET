2198585daf8d0115c5a4c9eeab3244fad3375dc4
henriquebelotto <henriquebelotto@gmail.com>
2019-06-18 10:12:48 -0400

    Added tooltip to the delete button. Added new version of Popper

--- MyRents/App_Start/BundleConfig.cs
+++ MyRents/App_Start/BundleConfig.cs
@@ -20,7 +20,7 @@ namespace MyRents
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.bundle.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap_flatly.css",
