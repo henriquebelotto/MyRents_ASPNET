2381805fbc3d73432708ebd23e8f0b7d7f198a00
henriquebelotto <henriquebelotto@gmail.com>
2019-07-08 18:32:13 -0400

    Added CreateNewRental method inside NewRentalsController

--- MyRents/Controllers/Api/NewRentalsController.cs
+++ MyRents/Controllers/Api/NewRentalsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -21,7 +22,32 @@ namespace MyRents.Controllers.Api
         [HttpPost]
         public IHttpActionResult CreateNewRental(NewRentalDto newRental)
         {
-            throw new NotImplementedException();
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == newRental.CustomerId);
+
+            if (customer == null)
+            {
+                return BadRequest("Invalid Customer ID");
+            }
+
+            // Loading multiple movies
+            // SELECT * from Movies Where Id in (1, 2, 3..);
+            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id));
+
+            foreach (var movie in movies)
+            {
+                Rental rental = new Rental
+                {
+                    Movie = movie,
+                    Customer = customer,
+                    DateRented = System.DateTime.Now
+                };
+                _context.Rentals.Add(rental);
+            }
+
+            _context.SaveChangesAsync();
+
+            // Using OK method because multiple resources, and the Create method returns the URL of a created resource
+            return Ok();
         }
     }
 }
