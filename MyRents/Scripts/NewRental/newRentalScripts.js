c4c51d83d2dc52b972e2bf8bfe173d846aa90a85
henriquebelotto <henriquebelotto@gmail.com>
2019-07-12 10:25:19 -0400

    added validator to newRental view. Error with typeAhead

--- MyRents/Scripts/NewRental/newRentalScripts.js
+++ MyRents/Scripts/NewRental/newRentalScripts.js
@@ -1,5 +1,6 @@
 ﻿$(document).ready(function () {
 
+
     var vm = {
         movieIds: []
     }; // viewModel
@@ -7,7 +8,6 @@
     var customers = new Bloodhound({
         datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
         queryTokenizer: Bloodhound.tokenizers.whitespace,
-        //prefetch: '../data/films/post_1960.json',
         remote: {
             url: '/api/customers?query=%QUERY',
             wildcard: '%QUERY'
@@ -16,7 +16,7 @@
 
     $('#customer').typeahead({
         minLength: 1,
-        highlight: true
+        highlight: true,
     }, {
             name: 'customers',
             display: 'name',
@@ -57,28 +57,58 @@
                 // store the value inside the movieIds array
                 vm.movieIds.push(movie.id);
 
-        });
+            });
 
     // Handling form submit
-    
-    $('#newRental').submit(function(e) {
-        // Preventing to have a traditional HTML form
-        e.preventDefault();
 
-        console.log(vm);
+    // Enforcing that a valid customer will be selected
+    $.validator.addMethod("validCustomer",
+        function () {
+            // make sure that the viewModel (vm) has a property called customerId and it's not zero
+            return vm.customerId && vm.customerId !== 0;
+        }, "Please, select a valid customer from the list");
 
-        // using AJAX
-        $.ajax({
+    // Adding client-side validation with JQUery
+    $("#newRental").validate({
+        submitHandler: function () {
+            // If this function is valid, it will call the submit method for the form
+            e.preventDefault();
+
+            // using AJAX
+            $.ajax({
                 url: "/api/newRentals",
                 method: "post",
                 data: vm
             })
-            .done(function () {
-                toastr.success("Rentals successfully recorded.");
-            })
-            .fail(function () {
-                toastr.fail("Something unexpected happened!");
-            });
+                .done(function () {
+                    toastr.success("Rentals successfully recorded.");
+                })
+                .fail(function () {
+                    toastr.fail("Something unexpected happened!");
+                });
+        }
     });
+
+    $("#newRental").validate();
+    // Submit method move inside the validation method
+    //$('#newRental').submit(function(e) {
+    //    // Preventing to have a traditional HTML form
+    //    e.preventDefault();
+
+    //    console.log(vm);
+
+    //    // using AJAX
+    //    $.ajax({
+    //            url: "/api/newRentals",
+    //            method: "post",
+    //            data: vm
+    //        })
+    //        .done(function () {
+    //            toastr.success("Rentals successfully recorded.");
+    //        })
+    //        .fail(function () {
+    //            toastr.fail("Something unexpected happened!");
+    //        });
+    //});
 });
 
