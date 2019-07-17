3e3b37facbc4a6c922d9db6cf0b0073a9bc05c2a
henriquebelotto <henriquebelotto@gmail.com>
2019-07-17 13:35:05 -0400

    added some security features and error handling pages

--- MyRents/App_Start/Startup.Auth.cs
+++ MyRents/App_Start/Startup.Auth.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
@@ -55,8 +56,8 @@ namespace MyRents
             //   consumerSecret: "");
 
             app.UseFacebookAuthentication(
-               appId: "secret",
-               appSecret: "secret");
+               appId: ConfigurationManager.AppSettings["FacebookAppId"],
+               appSecret: ConfigurationManager.AppSettings["FacebookSecret"]);
 
             app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             {
